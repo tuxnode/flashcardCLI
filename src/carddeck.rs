29cf6028d3abd5fb2d3e96358ca03e1f5d7eeba0
tuxnode <tuxnode@outlook.com>
2026-07-10 21:30:01 +0800

    feat: implement CardDeck new()

--- src/carddeck.rs
+++ src/carddeck.rs
@@ -1,5 +1,5 @@
-use std::fs;
-use std::io;
+use std::fs::File;
+use std::io::BufReader;
 
 use crate::flashcard::FlashCard;
 
@@ -11,7 +11,16 @@ pub struct CardDeck {
 impl CardDeck {
     // Load cards from JSON file, parsing to Vector
     // Return cards and file_path
-    pub fn new(path: &str) -> Result<Self, Box<dyn std::error::Error>> {}
+    pub fn new(path: &str) -> Result<Self, Box<dyn std::error::Error>> {
+        let file = File::open(path)?;
+        let reader = BufReader::new(file);
+
+        let cards: Vec<FlashCard> = serde_json::from_reader(reader)?;
+        Ok(CardDeck {
+            cards: cards,
+            file_path: path.to_string(),
+        })
+    }
 
     // Write cards information back to JSON file
     pub fn save() -> Result<(), Box<dyn std::error::Error>> {}
