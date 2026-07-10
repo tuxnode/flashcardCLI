1740e0e0a333e5386e7a68a8ce42436e80b13eab
tuxnode <tuxnode@outlook.com>
2026-07-10 16:44:53 +0800

    fix: add file_path check

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,7 @@
 // CLI Config Code
 
+use std::{fmt::format, path::Path};
+
 pub struct Args {
     pub mode: String,
     pub file_path: String,
@@ -11,18 +13,28 @@ impl Args {
 
         match args.len() {
             1 => Err(String::from("Usage: flashcard <mode> filepath")),
-            2 => Ok(Self {
-                mode: String::from("random"),
-                file_path: args[1].clone(),
-            }),
+            2 => {
+                let file_path = args[1].clone();
+                if !Path::new(&file_path).exists() {
+                    return Err(format!("Invalid path: {}", file_path));
+                }
+                Ok(Self {
+                    mode: String::from("random"),
+                    file_path: file_path,
+                })
+            }
             3 => {
                 let mode = args[1].clone();
+                let file_path = args[2].clone();
                 if mode != "random" && mode != "ordered" {
                     return Err(format!("Invalid mode {}: random, ordered", mode));
                 }
+                if !Path::new(&file_path).exists() {
+                    return Err(format!("Invalid path: {}", file_path));
+                }
                 Ok(Self {
                     mode: mode,
-                    file_path: args[2].clone(),
+                    file_path: file_path,
                 })
             }
             _ => Err(String::from("Too many arguments")),
